537ee475f590e3ecd6ce9bcaa330446881668c3e
Juan Daniel Galarza Rodriguez <juan.daniel.galarza@hotmail.com>
2020-02-13 16:35:49 -0500

    update menu-items to navigate withRouter HighOrderComponent0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const hatpage = () => (
+  <div>
+    <h1>HEllo wolds</h1>
+  </div>
+)
+
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/shop/hats' component={hatpage}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
